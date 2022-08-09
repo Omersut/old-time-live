89848aac3eb110867a7a8951a1779f3614372d83
omerSut <omer.sut@santsg.com>
2022-08-09 10:45:12 +0300

    TEST

--- src/App.js
+++ src/App.js
@@ -38,7 +38,9 @@ function App() {
   const [videoData, setVideoData] = useState("")
   const [isOpen, setIsOpen] = useState(false)
   const [category, setCategory] = useState("Music")
- 
+  const icons =[<FaHeadphonesAlt size={60} />, <BsCameraReels size={60} />, <TbMicrophone2 size={60} />, <BsNewspaper size={60} /> ]
+  const categories = ["Music", "Movies & Series", "Talk Show", "News"];
+  const years = [80,90,0]
   const opts = {
     width: "100%",
     height: "100%",
@@ -48,92 +50,94 @@ function App() {
       autoplay: 1,
     },
   };
+  const [videoDiv, setVideoDiv] = useState(<YouTube
+    className="youtube"
+    videoId={video}
+    opts={opts}
+    onReady={(e) => setVideoData(e.target)}
+  />)
+  const [change, setChange] = useState(false)
   const Change = () => {
-    if(year == 80){
-      if(category == "Music"){
-        let random = Math.floor(Math.random() * documentry.eighties.music.length);
-        setVideo(documentry.eighties.music[random])
-      }
-    }
-    if(year == 90){
-      if(category == "Music"){
-        let random = Math.floor(Math.random() * documentry.nineties.music.length);
-        setVideo(documentry.nineties.music[random])
-        console.log(video, random)
-
-      }
-    }
-    if(year == 0){
-      if(category == "Music"){
-        let random = Math.floor(Math.random() * documentry.noughties.music.length);
-        setVideo(documentry.noughties.music[random])
-      }
-    }
+   setChange(!change)
   }
-
+    
+  
   useEffect(() => {
-    Change();
-  }, [year])
+  let random = Math.floor(Math.random() * (year == 80 ? documentry.eighties.music.length : year == 90 ? documentry.nineties.music.length: documentry.noughties.music.length))
+  setVideo("");
+
+  setTimeout(() => {
+    setVideoDiv(<YouTube
+      className="youtube"
+      videoId={year == 80 ? documentry.eighties.music[random] : year == 90 ? documentry.nineties.music[random] : documentry.noughties.music[random]}
+      opts={opts}
+      onReady={(e) => setVideoData(e.target)}
+    />)
+  }, 1000);
+  }, [year,change])
+  
   
   return (
     <div className="App">
+      <div className="controls">
        
-        <div className="options" >
-
-          <div className={"option" + `${year == 80 ? " active": ""}`}  onClick={() => setYear(80) }>
-          {year == 80 & isOpen  && <YouTube className='youtube' videoId={video} opts={opts} onReady={(e)=> setVideoData(e.target)} />}
+       <div className="categories">
+       {categories.map((c,i) => (
+  <button
+  onClick={() => {
+    setCategory(c);
+  }}
+  className={"btn" + `${category == c ? " activeCategory" : ""}`}
+>
+ {icons[i]}
+</button>
+        ))}
+       </div>
+       <div className="control">
+          <button
+            className={"change" + `${!isOpen ? " start" : ""}`}
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            {isOpen ? <FiPause /> : <BiRightArrow />}{" "}
+          </button>
+          <button className="change" onClick={Change}>
+            <FaExchangeAlt onClick={Change} />
+          </button>
+        </div>
+     
+      </div>
+      <div className="options">
+        {years.map((y) => (
+          <div
+            className={"option" + `${year == y ? " active" : ""}`}
+            onClick={() => setYear(y)}
+          >
+            {(year == y) & isOpen && (
+              videoDiv
+            )}
             <div className="shadow" />
             <div className="label">
               <div className="icon">
-                <i className="fas fa-walking" >80s</i>
+                <i className="fas fa-walking">{y == 0 ? "00": y}s</i>
               </div>
               <div className="info">
-              <div className="main">{isOpen && category}</div>
-                <div className="sub">{isOpen && videoData.videoTitle && videoData.videoTitle.length > 35 ? videoData.videoTitle.slice(0, 35) + "...": videoData.videoTitle}</div>
+                <div className="main">{isOpen && category}</div>
+                <div className="sub">
+                  {isOpen &&
+                  videoData.videoTitle &&
+                  videoData.videoTitle.length > 35
+                    ? videoData.videoTitle.slice(0, 35) + "..."
+                    : videoData.videoTitle}
+                </div>
               </div>
             </div>
           </div>
-          <div className={"option" + `${year == 90 ? " active": ""}`} onClick={() => setYear(90) }>
-          {year == 90  & isOpen && <YouTube className='youtube' videoId={video} opts={opts} onReady={(e)=> setVideoData(e.target)} />}
-            <div className="shadow" />
-            <div className="label">
-              <div className="icon">
-                <i className="fas fa-snowflake">90s</i>
-              </div>
-              <div className="info">
-              <div className="main">{isOpen && category}</div>
-              <div className="sub">{isOpen && videoData.videoTitle && videoData.videoTitle.length > 35 ? videoData.videoTitle.slice(0, 35) + "...": videoData.videoTitle}</div>
-              </div>
-            </div>
-          </div>
-          <div className={"option" + `${year == 0 ? " active": ""}`} onClick={() => setYear(0) }>
-          {year == 0 & isOpen  && <YouTube className='youtube' videoId={video} opts={opts} onReady={(e)=> setVideoData(e.target)} />}
-            <div className="shadow" />
-            <div className="label">
-              <div className="icon">
-                <i className="fas fa-tree">00s</i>
-              </div>
-              <div className="info">
-              <div className="main">{isOpen && category}</div>
-              <div className="sub">{isOpen && videoData.videoTitle && videoData.videoTitle.length > 35 ? videoData.videoTitle.slice(0, 35) + "...": videoData.videoTitle}</div>
-              </div>
-            </div>
-          </div>
-        
-        </div>
-        <div className='controls'>
-          <div className='control'>
-         <button className={"change" + `${!isOpen ? " start": ""}`}  onClick={() => {setIsOpen(!isOpen)}}>{isOpen ? <FiPause/>: <BiRightArrow/> } </button>
-         <button className='change' onClick={Change}><FaExchangeAlt/></button>
-          </div>
-         <button onClick={()=> {setCategory("Music")}} className={"btn" + `${category == "Music" ? " activeCategory": ""}`}><FaHeadphonesAlt size={60}/> </button>
-         <button onClick={()=> {setCategory("Movies & Series")}} className={"btn" + `${category == "Movies & Series"  ? " activeCategory": ""}`}><BsCameraReels size={60}/> </button>
-         <button onClick={()=> {setCategory("Talk Show")}} className={"btn" + `${category == "Talk Show" ? " activeCategory": ""}`}><TbMicrophone2 size={60}/> </button>
-         <button onClick={()=> {setCategory("News")}} className={"btn" + `${category == "News" ? " activeCategory": ""}`}><BsNewspaper size={60}/> </button>         
-        </div>
-
-        </div>
-   
+        ))}
+      </div>
+      <div className="just">Just PC</div>
+    </div>
   );
 }
 
