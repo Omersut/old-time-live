b92437eeff5899c81dc2f7b7ede694d607820e0e
Ömer Süt <75551226+Omersut@users.noreply.github.com>
2022-08-11 00:18:42 +0300

    documentry

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ function App() {
   
   return (
     <div className="App">
-      <Control categories={categories} setCategory={setCategory} category={category} isOpen={isOpen} setIsOpen={setIsOpen} Change={Change} setChange={setChange} change={change}/>
+      <Control documentry={documentry} categories={categories} setCategory={setCategory} category={category} isOpen={isOpen} setIsOpen={setIsOpen} Change={Change} setChange={setChange} change={change}/>
       <Tv years={years} isOpen={isOpen} setYear={setYear} year={year} videoDiv={videoDiv} videoData={videoData} category={category}  />
       <div className="just">Just PC</div>
     </div>
