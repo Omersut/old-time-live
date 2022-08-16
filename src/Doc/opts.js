8e0d502a5800337382f825a65d3102aa3ed98608
Ömer Süt <75551226+Omersut@users.noreply.github.com>
2022-08-16 15:51:42 +0300

    sdf

--- src/Doc/opts.js
+++ src/Doc/opts.js
@@ -1,12 +1,13 @@
 const opts = {
-    width: "100%",
-    height: "100%",
-    transform: "scale(1.3)",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-      start: 25,
-      modestbranding: 1,
-    },
-  };
-  export default opts
+  width: "100%",
+  height: "100%",
+  transform: "scale(1.3)",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+    start: 25,
+    modestbranding: 1,
+    vq: "medium",
+  },
+};
+export default opts;
